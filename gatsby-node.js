6c4b4b3aed35b4aed337e749e96f42942dca4396
John <johnsmilga@gmail.com>
2018-12-27 21:59:32 -0800

    trying offical docs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,70 +1,57 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const _ = require(`lodash`)
+const Promise = require(`bluebird`)
+const path = require(`path`)
+const slash = require(`slash`)
 
-// You can delete this file if you're not using it
-
-const path = require('path')
-
+// Implement the Gatsby API “createPages”. This is
+// called after the Gatsby bootstrap is finished so you have
+// access to any information necessary to programmatically
+// create pages.
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return new Promise((resolve, reject) => {
-    const storeTemplate = path.resolve('src/pages/sample.js')
-    resolve(
-      graphql(`
+    // The “graphql” function allows us to run arbitrary
+    // queries against the local Contentful graphql schema. Think of
+    // it like the site has a built-in database constructed
+    // from the fetched data that you can run queries against.
+    graphql(
+      `
         {
           allContentfulProperties {
             edges {
               node {
                 id
-                description {
-                  description
-                }
-                neighborhood {
-                  neighborhood
-                }
-                features {
-                  content
-                }
-                images {
-                  fluid(maxWidth: 800) {
-                    src
-                    aspectRatio
-                    srcSet
-                    sizes
-                  }
-                }
-                title
-                address
-                type
-                price
-                bedrooms
-                bathrooms
-                garages
-                city
-                footage
-                featured
               }
             }
           }
         }
-      `).then(result => {
-        if (result.errors) {
-          reject(result.errors)
-        }
-        result.data.allContentfulProperties.edges.forEach(edge => {
-          createPage({
-            path: `/properties/${edge.node.id}`,
-            component: storeTemplate,
-            context: {
-              property: edge.node,
-            },
-          })
+      `
+    ).then(result => {
+      if (result.errors) {
+        reject(result.errors)
+      }
+
+      // Create Product pages
+      const productTemplate = path.resolve(`./src/templates/property.js`)
+      // We want to create a detailed page for each
+      // product node. We'll just use the Contentful id for the slug.
+      _.each(result.data.allContentfulProperties.edges, edge => {
+        // Gatsby uses Redux to manage its internal state.
+        // Plugins and sites can use functions like "createPage"
+        // to interact with Gatsby.
+        createPage({
+          // Each page is required to have a `path` as well
+          // as a template component. The `context` is
+          // optional but is often necessary so the template
+          // can query data specific to each page.
+          path: `/properties/${edge.node.id}/`,
+          component: slash(productTemplate),
+          context: {
+            id: edge.node.id,
+          },
         })
-        return
       })
-    )
+      resolve()
+    })
   })
 }
